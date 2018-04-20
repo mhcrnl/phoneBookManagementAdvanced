68cae50e898b01d97716d46dbefead22ecffb5ad
Mihai Cornel <mhcrnl@gmail.com>
2018-04-20 08:35:49 +0300

    20.04.2018

---
+++ 02PhoneBook2018/src/pkg02phonebook2018/PhoneEntry.java
@@ -0,0 +1,68 @@
+package pkg02phonebook2018;
+
+import java.util.ArrayList;
+
+public class PhoneEntry {
+	private String name;
+	private ArrayList<String> phoneNumber = new ArrayList<String>();
+
+	public PhoneEntry() {
+	}
+
+	public PhoneEntry(String name, ArrayList<String> phoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber.add(phoneNumber);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<String> getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public boolean addPhoneNumber(String phoneNumber) {
+
+		ArrayList<String> entryNumber = new ArrayList<>(this.phoneNumber);
+		entryNumber.add(phoneNumber);
+		if (!isDuplicatePhoneNumber(entryNumber)) {
+			this.phoneNumber = entryNumber;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean deletePhoneNumber(String phoneNumber) {
+		if (this.phoneNumber.remove(phoneNumber)) {
+			return true;
+		} else
+		
+			return false;
+
+	}
+
+	private boolean isDuplicatePhoneNumber(ArrayList<String> phoneNumber) {
+		for (int i = 0; i < phoneNumber.size(); i++) {
+			for (int j = i + 1; j < phoneNumber.size(); j++) {
+				if (phoneNumber.get(i).equals(phoneNumber.get(j))) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public String toString() {
+		return "Name: " + name + ", Phone number: " + phoneNumber;
+	}
+
+}
